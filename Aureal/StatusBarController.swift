7a5390434f2901b69e8dd91683b9f0b5fb256d73
Aluveitie <aluveitie@gmail.com>
2021-10-06 18:08:45 +0200

    Adds popover to show/exit the App

--- Aureal/StatusBarController.swift
+++ Aureal/StatusBarController.swift
@@ -7,24 +7,56 @@
 
 import Foundation
 import AppKit
+import Cocoa
+import SwiftUI
 
 class StatusBarController {
     private var statusBar: NSStatusBar
     private var statusItem: NSStatusItem
     
+    private var popover: NSPopover
+    
     init() {
         statusBar = NSStatusBar.init()
         statusItem = statusBar.statusItem(withLength: 28.0)
         
+        popover = NSPopover.init()
+        let popupView = PopupView(popover: popover)
+        popover.contentSize = NSSize(width: 120, height: 32)
+        popover.contentViewController = NSHostingController(rootView: popupView)
+        
         if let statusBarButton = statusItem.button {
             statusBarButton.image = #imageLiteral(resourceName: "status")
             statusBarButton.image?.size = NSSize(width: 18.0, height: 18.0)
-            statusBarButton.action = #selector(toggleWindow(sender:))
+            statusBarButton.action = #selector(togglePopover(sender:))
             statusBarButton.target = self
         }
     }
     
-    @objc func toggleWindow(sender: AnyObject) {
+    @objc func togglePopover(sender: AnyObject) {
+        if (popover.isShown) {
+            popover.performClose(sender)
+        } else {
+            if let statusBarButton = statusItem.button {
+                popover.show(relativeTo: statusBarButton.bounds, of: statusBarButton, preferredEdge: NSRectEdge.maxY)
+            }
+        }
+    }
+}
+
+struct PopupView: View {
+    var popover: NSPopover
+    
+    var body: some View {
+        Button(action: { toggleWindow(); popover.close(); }) {
+            Text("Open").frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        Button(action: { exit(0) }) {
+            Text("Exit").frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+    }
+    
+    func toggleWindow() {
         let mainWindow = NSApplication.shared.windows.first!
         if (mainWindow.isVisible) {
             mainWindow.close()
