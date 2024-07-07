d268cca422dd34385a212c7c362158bfd22b2d31
quyiming <quyiming@1024ul.com>
2024-07-08 01:43:11 +0800

    feat: update default color

--- Aureal/View Controller/DeviceViewController.swift
+++ Aureal/View Controller/DeviceViewController.swift
@@ -28,8 +28,9 @@ class DeviceViewController: NSViewController {
     var selectedDevice: AuraUSBDevice?
 
     let defaultPalette = [NSColor]([
-        .goodRed,
-        .blue,
+        NSColor(red: 0 / 255, green: 255 / 255, blue: 0 / 255, alpha: 1),
+        NSColor(red: 0 / 255, green: 255 / 255, blue: 0 / 255, alpha: 1),
+//        NSColor(red: 255 / 255, green: 218 / 255, blue: 255 / 255, alpha: 1),
     ])
 
     private var currentEffect: Effect!
